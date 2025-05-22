a253352b2b3dd7fcd8bcf516fa21835462c99067
Doyun Kim <82309982+ehdbs0903@users.noreply.github.com>
2025-05-22 09:25:30 +0900

    feat(player): freelook camera

--- Assets/Scripts/Doyun/Player/PlayerMovement.cs
+++ Assets/Scripts/Doyun/Player/PlayerMovement.cs
@@ -1,18 +1,32 @@
 using Fusion;
 using Fusion.Addons.KCC;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour {
-    private NetworkBool _canMove { get; set; } = true;
+    // private NetworkBool _canMove { get; set; } = true;
+    [SerializeField] private GameObject freeLookPrefab;
+    [SerializeField] private Transform cameraPivot;
+    
+    private GameObject _camInstance;
+    
     private KCC _cc;
     private Vector3 _dir;
-    private NetworkButtons _prev;
+    
+    // private NetworkButtons _prev;
 
     [Networked] private TickTimer _moveTimer { get; set; }
 
-    public override void Spawned()
-    {
+    public override void Spawned() {
         _cc = GetComponent<KCC>();
+
+        if (Object.HasInputAuthority) {
+            _camInstance = Instantiate(freeLookPrefab);
+            var freeLook = _camInstance.GetComponent<CinemachineCamera>();
+            
+            freeLook.Follow = cameraPivot;
+            freeLook.LookAt = cameraPivot;
+        }
     }
 
     public override void FixedUpdateNetwork()
@@ -20,16 +34,23 @@ public class PlayerMovement : NetworkBehaviour {
         if (Object.IsProxy) return;
         
         // UpdateMoveCD();
+
+        Vector3 camForward = _camInstance.transform.forward;
+        Vector3 camRight = _camInstance.transform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
         
         if (GetInput<MyNetworkInput>(out var input))
         {
             _dir = Vector3.zero;
 
-            if (input.IsDown(MyNetworkInput.BUTTON_FORWARD)) _dir += Vector3.forward;
-            else if (input.IsDown(MyNetworkInput.BUTTON_BACKWARD)) _dir -= Vector3.forward;
+            if (input.IsDown(MyNetworkInput.BUTTON_FORWARD)) _dir += camForward;
+            else if (input.IsDown(MyNetworkInput.BUTTON_BACKWARD)) _dir -= camForward;
 
-            if (input.IsDown(MyNetworkInput.BUTTON_RIGHT)) _dir += Vector3.right;
-            else if (input.IsDown(MyNetworkInput.BUTTON_LEFT)) _dir -= Vector3.right;
+            if (input.IsDown(MyNetworkInput.BUTTON_RIGHT)) _dir += camRight;
+            else if (input.IsDown(MyNetworkInput.BUTTON_LEFT)) _dir -= camRight;
             
             _cc.SetInputDirection(_dir.normalized);
 
@@ -38,7 +59,7 @@ public class PlayerMovement : NetworkBehaviour {
                 Quaternion rot = Quaternion.LookRotation(_dir);
                 _cc.SetLookRotation(rot);
             }
-            _prev = input.Buttons;
+            // _prev = input.Buttons;
         }
     }
     
