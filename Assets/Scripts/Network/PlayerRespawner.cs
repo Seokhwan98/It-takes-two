d8efa68e0ec2b8f082ea532726dc74d5b4dfc19f
Doyun Kim <82309982+ehdbs0903@users.noreply.github.com>
2025-06-04 10:58:25 +0900

    feat(lobby): player respawn

--- Assets/Scripts/Network/PlayerRespawner.cs
+++ Assets/Scripts/Network/PlayerRespawner.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class PlayerRespawner : MonoBehaviour
 {
-    [SerializeField] private Transform respawnPoint;
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponentInParent<NetworkObject>() == null)
-        { 
-            Debug.Log($"PlayerRespawner: OnTriggerEnter with {other.name}");
             return;
-        }
 
         if (other.GetComponentInParent<NetworkObject>().HasStateAuthority)
         {
             var cc = other.GetComponentInParent<KCC>();
             RespawnPlayer(cc);
-            Debug.Log("1");
         }
     }
-    
+
     private void RespawnPlayer(KCC cc)
     {
-        cc.SetPosition(respawnPoint.position);
-        cc.SetLookRotation(respawnPoint.rotation);
+        if (cc.HasStateAuthority)
+        {
+            var respawnPoint = transform;
+            
+            cc.TeleportRPC(respawnPoint.position, respawnPoint.rotation.eulerAngles.y,
+                respawnPoint.rotation.eulerAngles.x);
+        }
     }
 }
