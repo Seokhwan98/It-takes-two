45170c08885ffe4c40a39a191af5057ddef6da00
Seokhwan98 <seokhwan98@naver.com>
2025-06-07 20:10:40 +0900

    ADD: 룸 씬 수정

--- Assets/Scripts/Network/Room/RoomStarter.cs
+++ Assets/Scripts/Network/Room/RoomStarter.cs
@@ -11,32 +11,29 @@ public class RoomStarter : NetworkBehaviour {
     public override void Spawned()
     {
         InterfaceManager.Instance.MouseEnable();
-        Debug.Log("11");
     }
 
     public void AutoHostOrClientSpawn()
     {
-        Debug.Log("22");
         bool isHost = Runner.SessionInfo.PlayerCount == 1;
-        Vector3 SpawnPoint = isHost ? _spawnPointA.position : _spawnPointB.position;
+        var spawnPoint = isHost ? _spawnPointA : _spawnPointB;
+        var spawnPosition = spawnPoint.position;
+        var spawnRotation = spawnPoint.rotation;
         
         foreach (PlayerRef playerRef in Runner.ActivePlayers)
         {
             if (Runner.SessionInfo.PlayerCount == 2 && playerRef == Runner.LocalPlayer)
                 continue;
             
-            Debug.Log("33");
-            var playerObject = Runner.Spawn(_playerPrefab, SpawnPoint, inputAuthority: playerRef);
-            Debug.Log("44");
+            var playerObject = Runner.Spawn(_playerPrefab, spawnPosition, spawnRotation, inputAuthority: playerRef);
+            
             RPC_ComponentDisabled(playerObject);
-            Debug.Log("66");
         }
     }
     
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void RPC_ComponentDisabled(NetworkObject playerObject)
     {
-        Debug.Log("55");
         playerObject.GetComponent<PlayerMovement>().enabled = false;
         playerObject.GetComponent<KCC>().enabled = false;
     }
