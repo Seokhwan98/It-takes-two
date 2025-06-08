c67cf2611f2f8f3ebdcb60f1b9db3aac8f1c0483
Starbow-Break <starbow.dev@gmail.com>
2025-06-08 15:49:09 +0900

    ADD : BGM 볼륨 설정 UI 추가

--- Assets/Scripts/Audios/SoundType.cs
+++
@@ -1,5 +0,0 @@
-public enum SoundType
-{
-    PICKUP,
-    PICKDOWN
-}
--- Assets/Scripts/Audios/Volume.cs
+++ Assets/Scripts/Audios/Volume.cs
@@ -2,23 +2,7 @@ using UnityEngine;
 
 public static class Volume
 {
-    public static float MasterVolume = 0.5f;
     public static float BGMVolume = 0.5f;
-    public static float SFXVolume = 0.5f;
-
-    public enum VolumeType
-    {
-        Master, 
-        BGM, 
-        SFX,
-    }
-    
-    public static void SaveMasterVolumes(float volume)
-    {
-        MasterVolume = volume;
-        PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
-        PlayerPrefs.Save();
-    }
     
     public static void SaveBGMVolumes(float volume)
     {
@@ -26,18 +10,9 @@ public static class Volume
         PlayerPrefs.SetFloat("BGMVolume", BGMVolume);
         PlayerPrefs.Save();
     }
-    
-    public static void SaveSFXVolumes(float volume)
-    {
-        SFXVolume = volume;
-        PlayerPrefs.SetFloat("SFXVolume", SFXVolume);
-        PlayerPrefs.Save();
-    }
 
     public static void LoadVolumes()
     {
-        MasterVolume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
         BGMVolume = PlayerPrefs.GetFloat("BGMVolume", 0.5f);
-        SFXVolume = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
     }
 }
